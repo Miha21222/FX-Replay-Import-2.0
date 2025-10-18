29aecff916d8f65260cbf17386607e48daf71589
Kompotus <67239835+Miha21222@users.noreply.github.com>
2025-10-18 15:19:59 +0300

    fix: models.py for deployment

--- src/app/database/models.py
+++ src/app/database/models.py
@@ -6,9 +6,9 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 psql_engine = create_async_engine("postgresql+asyncpg://user:pass@localhost/db_name", echo=True)
 # Для докера 🔽
-# sqlite_engine = create_async_engine("sqlite+aiosqlite:////src/users.db", echo=True)
+sqlite_engine = create_async_engine("sqlite+aiosqlite:////src/users.db", echo=True)
 # Для локального запуска 🔽
-sqlite_engine = create_async_engine("sqlite+aiosqlite:///src/users.db", echo=True)
+# sqlite_engine = create_async_engine("sqlite+aiosqlite:///src/users.db", echo=True)
 
 async_sqlite_session = async_sessionmaker(sqlite_engine)
 async_psql_session = async_sessionmaker(psql_engine)
